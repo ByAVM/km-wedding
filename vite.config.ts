38636f787aefe17a3f87b51795346cb8006ff8b8
ByAVM <miha_102035_20@mail.ru>
2026-05-19 17:20:15 +0000

    Настройка CI

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: process.env.IS_CI ? '/km-wedding/' : '/',
   plugins: [
     tailwindcss(),
   ],
